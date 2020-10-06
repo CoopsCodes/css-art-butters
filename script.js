a7f4a5e76ecad4e193b6c26ef7616eb802d4cb65
Cooper Viktor <im.cooperviktor@gmail.com>
2020-10-06 12:27:26 +1100

    dropdown selectot targeting right value

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const showButton = document.getElementById("rgbButton");
 const rgbSelector = document.getElementById("rgb");
+const articleSelector = document.getElementById("item-select");
+
+// const opt = document.getElementsByTagName("option")[articleSelector].value;
+//
 
 // Toggle the RGB selector
 showButton.addEventListener("click", () => {
@@ -8,4 +12,8 @@ showButton.addEventListener("click", () => {
 	} else {
 		rgbSelector.style.display = "block";
 	}
+});
+
+articleSelector.addEventListener("change", (e) => {
+	document.getElementById("selectedItem").innerHTML = articleSelector.value;
 });
